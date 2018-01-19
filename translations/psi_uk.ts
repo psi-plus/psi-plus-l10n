78e9871cad10dbbad0d931d53c0b3b436ebbb764
Boris Pek <tehnick-8@yandex.ru>
2018-01-19 17:35:18 +0300

    Translations were updated from Transifex.

--- translations/psi_uk.ts
+++ translations/psi_uk.ts
@@ -8298,15 +8298,15 @@ Tags: %1</source>
     </message>
     <message>
         <source>Keep accounts&apos; passwords in more secure system keyring manager instead of profile settings.</source>
-        <translation type="unfinished"/>
+        <translation>Зберігати паролі облікових записів в більш безпечному системному менеджері паролів замість налаштувань профілю.</translation>
     </message>
     <message>
         <source>Use system keyring manager</source>
-        <translation type="unfinished"/>
+        <translation>Використовувати системний менеджер паролів</translation>
     </message>
     <message>
         <source>99999</source>
-        <translation type="unfinished"/>
+        <translation>99999</translation>
     </message>
 </context>
 <context>
